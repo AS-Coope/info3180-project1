22842d2a510215dc34eb0bb335b8aee38f99e137
AS-Coope <deltazirconium@gmail.com>
2024-03-09 17:46:26 -0500

    create property model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
 from . import db
+
+class Property():
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100))
+    num_of_bedrooms = db.Column(db.Integer)
+    num_of_bathrooms = db.Column(db.Integer)
+    location = db.Column(db.String(90))
+    price = db.Column(db.DECIMAL(11,2))
+    type = db.Column(db.String(50))
+    description = db.Column(db.String(200))
+    filename = db.Column(db.String(200))
