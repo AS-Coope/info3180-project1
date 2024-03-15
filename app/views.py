4f802d45dfb8198aa57c47e410a834c58981506a
AS-Coope <deltazirconium@gmail.com>
2024-03-15 14:24:53 -0500

    import Property model from models

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ This file contains the routes for your application.
 from app import app
 from flask import render_template, flash, request, redirect, url_for
 from app.forms import PropertyForm
+from app.models import Property
 
 
 ###
