c035448048676db005cad453d4975c8a4023bc0d
AS-Coope <deltazirconium@gmail.com>
2024-03-15 11:10:03 -0500

    setup form file

---
+++ app/forms.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField, SelectField
+from wtforms.validators import InputRequired
